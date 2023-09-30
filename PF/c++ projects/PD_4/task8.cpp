c96cb8f877c15ce1786866a293c05c24cb42bfc7
omer ghazali <77153164+MuhammadOmerGhazali@users.noreply.github.com>
2023-09-30 21:44:16 +0500

    PD_4_2

--- PF/c++ projects/PD_4/task8.cpp
+++ PF/c++ projects/PD_4/task8.cpp
@@ -3,35 +3,35 @@ using namespace std;
 int holidays(int holiday);
 int main()
 {
-int holiday;
-cout<<"Holidays: ";
-cin>>holiday;
-holidays(holiday);
+	int holiday;
+	cout<<"Holidays: ";
+	cin>>holiday;
+	holidays(holiday);
 }
 int holidays(int holiday)
 {
-int working_day;
-working_day=365-holiday;
-int games;
-games =((working_day*63)+(holiday*127));
+	int working_day;
+	working_day=365-holiday;
+	int games;
+	games =((working_day*63)+(holiday*127));
 
-int hour,minute,norm;
-norm=30000-games;
-if (games>30000)
+	int hour,minute,norm;
+	norm=30000-games;
+	if (games>30000)
 {
 			
-	hour=(norm/60)*-1;
-	minute=(norm%60)*-1;
-	cout<<"Tom will run away"<<endl<<hour<<" hours and "<<minute<<" minutes for play";
+		hour=(norm/60)*-1;
+		minute=(norm%60)*-1;
+		cout<<"Tom will run away"<<endl<<hour<<" hours and "<<minute<<" minutes for play";
 
 
 }
-if (games<30000)
+	if (games<30000)
 {	
 		
-	hour=norm/60;
-	minute=(norm%60);
-	cout<<"Tom sleeps well"<<endl<<hour<<" hours and "<<minute<<" minutes less for play";
+		hour=norm/60;
+		minute=(norm%60);
+		cout<<"Tom sleeps well"<<endl<<hour<<" hours and "<<minute<<" minutes less for play";
 
 }
 }
